724b5ae7562291b41f6069965ec5980c37da5ecb
ita-jp <tn00gm@gmail.com>
2022-12-15 23:41:14 +0900

    第10章 テストに聞いてみる

--- wycash/src/test/java/money/MoneyTest.java
+++ wycash/src/test/java/money/MoneyTest.java
@@ -35,4 +35,9 @@ public class MoneyTest {
         assertThat(Money.dollar(1).currency()).isEqualTo("USD");
         assertThat(Money.franc(1).currency()).isEqualTo("CHF");
     }
+
+    @Test
+    public void testDifferentClassEquality() {
+        assertThat(new Money(10, "CHF")).isEqualTo(new Franc(10, "CHF"));
+    }
 }
